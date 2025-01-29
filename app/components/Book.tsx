98ff289525db11ec39ce583c95428f8d8f4614e0
arimu <arimurahiroaki40@gmail.com>
2025-01-29 23:40:17 +0900

    モーダルがうまく表示されないバグが発生中

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -3,16 +3,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BookType } from "../types/types";
+import { useState } from "react";
 
 type BookProps = {
   book: BookType;
-}
+};
 
-// eslint-disable-next-line react/display-name
 const Book = ({ book }: BookProps) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handlePurchaseClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
-      {/* アニメーションスタイル */}
       <style jsx global>{`
         @keyframes fadeIn {
           from {
@@ -29,8 +38,11 @@ const Book = ({ book }: BookProps) => {
         }
       `}</style>
 
-      <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+      <div className="relative flex flex-col items-center m-4">
+        <a
+          onClick={handlePurchaseClick}
+          className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+        >
           <Image
             priority
             src={book.thumbnail.url}
@@ -46,17 +58,29 @@ const Book = ({ book }: BookProps) => {
           </div>
         </a>
 
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
+        {showModal && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div
+              className="fixed inset-0 bg-slate-900 bg-opacity-50"
+              onClick={handleCancel}
+            />
+            <div className="relative z-50 bg-white p-8 rounded-lg modal">
+              <h3 className="text-xl mb-4">本を購入しますか？</h3>
+              <button
+                onClick={handlePurchaseClick}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+              >
+                購入する
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              >
+                キャンセル
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
