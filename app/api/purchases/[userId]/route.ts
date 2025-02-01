83390baba56a64174a9a9fa2dc4512b15382edb3
arimu <arimurahiroaki40@gmail.com>
2025-02-01 09:47:24 +0900

    エラーを修正

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,13 +1,15 @@
 import prisma from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // 購入履歴検索API
 export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
+  request: NextRequest,
+  context: { params: { userId: string } }
 ) {
-  console.log("userIdは" + params.userId);
-  const userId = params.userId;
+  const { params } = context; // `params` を context 経由で取得
+  const userId = params?.userId; // `?.` で安全にアクセス
+
+  console.log("userIdは" + userId);
 
   try {
     if (!userId) {
@@ -16,13 +18,15 @@ export async function GET(
         { status: 400 }
       );
     }
+
     const purchases = await prisma.purchase.findMany({
       where: {
         userId: userId,
       },
     });
+
     return NextResponse.json(purchases);
   } catch (err) {
-    return NextResponse.json(err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
